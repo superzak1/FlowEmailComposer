2a0620c834d59da808d60821cb90bf33dc1b94c4
Suraj Pillai <85.suraj@gmail.com>
2020-12-18 17:46:10 +0000

    Add Email send functionality to LWC

--- force-app/main/default/lwc/flowEmailComposerLwc/flowEmailComposerLwc.js
+++ force-app/main/default/lwc/flowEmailComposerLwc/flowEmailComposerLwc.js
@@ -1,8 +1,13 @@
 import { LightningElement, api, wire } from "lwc";
+import { FlowAttributeChangeEvent, FlowNavigationNextEvent } from "lightning/flowSupport";
 import getEmailTemplates from "@salesforce/apex/FlowEmailComposerCtrl.getEmailTemplates";
 import getTemplateDetails from "@salesforce/apex/FlowEmailComposerCtrl.getTemplateDetails";
+import sendAnEmailMsg from "@salesforce/apex/FlowEmailComposerCtrl.sendAnEmailMsg";
+import TickerSymbol from "@salesforce/schema/Account.TickerSymbol";
 
 export default class FlowEmailComposer extends LightningElement {
+  @api availableActions = [];
+
   @api whoId = null;
   @api whatId = null;
   uploadRefId = null;
@@ -14,9 +19,7 @@ export default class FlowEmailComposer extends LightningElement {
   selFolderId = null;
   filteredTemplateList = [{ label: "--Select a Template--", value: "select" }];
   @api subject = "";
-  folders = [
-    { label: "--Select an E-mail Template Folder--", value: "select" }
-  ];
+  folders = [{ label: "--Select an E-mail Template Folder--", value: "select" }];
   emailTemplates = [];
   recordError = "";
   @api filesTobeAttached = [];
@@ -33,15 +36,14 @@ export default class FlowEmailComposer extends LightningElement {
   @api hideTemplateSelection = false;
   @api logEmail = false;
   @api showSendButton = false;
+  @api sendOnNext = false;
 
   get showCCButton() {
     return this.showCCField && !this.ccAddresses;
   }
 
   get hasTemplateAttachments() {
-    return (
-      this.attachmentsFromTemplate && this.attachmentsFromTemplate.length > 0
-    );
+    return this.attachmentsFromTemplate && this.attachmentsFromTemplate.length > 0;
   }
 
   get showBCCButton() {
@@ -58,6 +60,41 @@ export default class FlowEmailComposer extends LightningElement {
 
   get showBCCFieldFinal() {
     return this.showBCCField || this.bccAddresses;
+  }
+
+  _updateFlowAttr(attrName, value) {
+    const attrEvent = new FlowAttributeChangeEvent(attrName, value);
+    this.dispatchEvent(attrEvent);
+  }
+
+  handleSubjectChange(event) {
+    this.subject = event.target.value;
+    this._updateFlowAttr("subject", this.subject);
+  }
+
+  handleBodyChange(event) {
+    this.emailBody = event.target.value;
+    this._updateFlowAttr("emailBody", this.emailBody);
+  }
+
+  handleToAddressesChange(event) {
+    this.toAddresses = event.target.value;
+    this._updateFlowAttr("toAddresses", this.toAddresses);
+  }
+
+  handleCCAddressesChange(event) {
+    this.ccAddresses = event.target.value;
+    this._updateFlowAttr("ccAddresses", this.ccAddresses);
+  }
+
+  handleBCCAddressesChange(event) {
+    this.bccAddresses = event.target.value;
+    this._updateFlowAttr("bccAddresses", this.bccAddresses);
+  }
+
+  updateTemplateId(event) {
+    this.emailTemplateId = event.target.value;
+    this._updateFlowAttr("emailTemplateId", this.emailTemplateId);
   }
 
   @wire(getEmailTemplates)
@@ -83,6 +120,8 @@ export default class FlowEmailComposer extends LightningElement {
   }
 
   changeBody(event) {
+    this.updateTemplateId(event);
+    this.showSpinner = true;
     this.selTemplateId = event.target.value;
     getTemplateDetails({
       templateId: this.selTemplateId,
@@ -95,6 +134,52 @@ export default class FlowEmailComposer extends LightningElement {
       })
       .catch((err) => {
         console.error(err);
+      })
+      .finally(() => {
+        this.showSpinner = false;
       });
+  }
+
+  _getSendParams() {
+    return {
+      fromAddress: this.fromAddress,
+      toAddressesStr: this.toAddresses,
+      ccAddressesStr: this.ccAddresses,
+      bccAddressesStr: this.bccAddresses,
+      subject: this.subject,
+      whoId: this.whoId,
+      whatId: this.whatId,
+      body: this.emailBody,
+      senderDisplayName: this.senderName,
+      contentDocumentIds: [],
+      attachmentIds: [],
+      createActivity: this.logEmail
+    };
+  }
+
+  async sendEmail() {
+    this.showSpinner = true;
+    const params = this._getSendParams();
+    try {
+      await sendAnEmailMsg(params);
+      console.log("message sent");
+    } catch (err) {
+      console.error("an error occurred", err);
+      throw err;
+    } finally {
+      this.showSpinner = false;
+    }
+  }
+
+  async handleGoNext() {
+    // check if NEXT is allowed on this screen
+    if (this.availableActions.find((action) => action === "NEXT")) {
+      // navigate to the next screen
+      if (this.sendOnNext === true) {
+        await this.sendEmail();
+      }
+      const navigateNextEvent = new FlowNavigationNextEvent();
+      this.dispatchEvent(navigateNextEvent);
+    }
   }
 }
